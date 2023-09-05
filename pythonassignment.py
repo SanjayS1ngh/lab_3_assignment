164ea2d893d5aa8961a887208b5a4f37af8c8842
SanjayS1gh <samantsanjay100@gmail.com>
2023-09-05 15:19:52 +0530

    cmt_msg_01

--- pythonassignment.py
+++ pythonassignment.py
@@ -1,16 +1,16 @@
 employee_data = [
-    {"Employee ID": "161E90", "Name": "Raman", "Age": 41, "Salary": 56000},
-    {"Employee ID": "161F91", "Name": "Himadri", "Age": 38, "Salary": 67500},
-    {"Employee ID": "161F99", "Name": "Jaya", "Age": 51, "Salary": 82100},
-    {"Employee ID": "171E20", "Name": "Tejas", "Age": 30, "Salary": 55000},
-    {"Employee ID": "171G30", "Name": "Ajay", "Age": 45, "Salary": 44000},
+    {"Employee ID": "161E90", "Name": "sanjay", "Age": 41, "Salary": 56000},
+    {"Employee ID": "161F91", "Name": "harshiti", "Age": 38, "Salary": 67500},
+    {"Employee ID": "161F99", "Name": "arshul", "Age": 51, "Salary": 82100},
+    {"Employee ID": "171E20", "Name": "sham", "Age": 30, "Salary": 55000},
+    {"Employee ID": "171G30", "Name": "ram", "Age": 45, "Salary": 44000},
 ]
 def sort_employee_data(choice):
-    if choice == 1:  # Sort by Age
+    if choice == 1:  
         sorted_data = sorted(employee_data, key=lambda x: x["Age"])
-    elif choice == 2:  # Sort by Name
+    elif choice == 2:  
         sorted_data = sorted(employee_data, key=lambda x: x["Name"])
-    elif choice == 3:  # Sort by Salary
+    elif choice == 3: 
         sorted_data = sorted(employee_data, key=lambda x: x["Salary"])
     else:
         print("Invalid choice. Please select a valid sorting parameter (1, 2, or 3).")
